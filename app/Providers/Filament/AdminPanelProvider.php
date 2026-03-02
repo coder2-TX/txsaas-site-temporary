ec37b1c643abc92abac85d92c45431d7431a06cb
Dhiaa <dhiaa@alshamy.com>
2026-03-03 01:18:42 +0300

    تعديلات بواجهه الفيلمينت

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->brandName('TX-SaaS')
             ->login()
             ->colors([
                 'primary' => Color::Blue,
